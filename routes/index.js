fb4b76252896d8f38e9cbf3230d3e38285767bc8
YU-CHE CHEN <job047587213@hotmail.com>
2020-09-12 01:27:13 +0800

    Add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,26 @@ const userController = require('../controllers/userController.js')
 
 module.exports = (app, passport) => {
 
-  app.get('/', (req, res) => res.redirect('/restaurants'))
-  app.get('/restaurants', restController.getRestaurants)
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
 
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin', authenticated, (req, res) => res.redirect('/admin/restaurants'))
+
+  app.get('/admin/restaurants', authenticated, adminController.getRestaurants)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
