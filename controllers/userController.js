37a92cdb2c95debe286551f99798552f48088bb3
YU-CHE CHEN <job047587213@hotmail.com>
2020-09-20 01:15:04 +0800

    feat: Create the user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+const fs = require('fs');
 
 const userController = {
   signUpPage: (req, res) => {
@@ -8,12 +11,10 @@ const userController = {
   },
 
   signUp: (req, res) => {
-    // confirm password
     if (req.body.passwordCheck !== req.body.password) {
       req.flash('error_messages', '兩次密碼輸入不同！')
       return res.redirect('/signup')
     } else {
-      // confirm unique user
       User.findOne({ where: { email: req.body.email } }).then(user => {
         if (user) {
           req.flash('error_messages', '信箱重複！')
@@ -45,7 +46,68 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
-  }
+  },
+
+  getUser: (req, res) => {
+    return User.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+    }).then((user) => {
+      return res.render('profile', {
+        user: user,
+      })
+    })
+  },
+
+
+  editUser: (req, res) => {
+    return User.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+    }).then((user) => {
+      return res.render('editProfile', {
+        user: user,
+      })
+    })
+  },
+
+  putUser: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "Name must be filled")
+      return res.redirect('back')
+    }
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        console.log(img)
+        return User.findByPk(req.params.id)
+          .then((user) => {
+            user.update({
+              name: req.body.name,
+              image: file ? img.data.link : user.image
+            })
+              .then((user) => {
+                req.flash('success_messages', 'restaurant was successfully to update')
+                res.redirect(`/users/${req.params.id}`)
+              })
+          })
+      })
+    }
+    else {
+      return User.findByPk(req.params.id)
+        .then((user) => {
+          user.update({
+            name: req.body.name,
+            image: user.image
+          })
+            .then((user) => {
+              req.flash('success_messages', 'user was successfully to update')
+              res.redirect(`/users/${req.params.id}`)
+            })
+        })
+    }
+  },
 
 }
 
